a30622024227ebc02013cd9acdbcdc107bb00e83
El-paolo <paolocordovacontacto@gmail.com>
2023-08-06 18:54:36 -0400

    added navbar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,7 +39,6 @@ class MoviesController < ApplicationController
   def update
     respond_to do |format|
       if @movie.update(movie_params)
-        git 
         
         format.html { redirect_to movie_url(@movie), notice: "Movie was successfully updated." }
         format.json { render :show, status: :ok, location: @movie }
